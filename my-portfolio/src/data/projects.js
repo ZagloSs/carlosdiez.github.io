cbaa75898aad501af6f877237c14066d4cc317a8
ZagloSs <carlosdiezmendez@gmail.com>
2025-04-25 13:49:27 +0200

    sup

---
+++ my-portfolio/src/data/projects.js
@@ -0,0 +1,25 @@
+export const proyectos = [
+    {
+        link: "#",
+        img:"src/imgs/mockup.png",
+        title: "Mockup",
+        timeSpent: "3 h",
+        bio: "biologica"
+    },
+
+    {
+        link: "#",
+        img:"src/imgs/mockup.png",
+        title: "Mockup2",
+        timeSpent: "3 h",
+        bio: "biologica"
+    },
+
+    {
+        link: "#",
+        img:"src/imgs/mockup.png",
+        title: "Mockup3",
+        timeSpent: "3 h",
+        bio: "biologica"
+    }
+];
