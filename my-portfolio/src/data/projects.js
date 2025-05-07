363a098ecb22f64f0c6e32cd8a8d70fea3890419
ZagloSs <carlosdiezmendez@gmail.com>
2025-05-07 14:28:24 +0200

    Sudoku solver project added

--- my-portfolio/src/data/projects.js
+++ my-portfolio/src/data/projects.js
@@ -1,19 +1,20 @@
 export const proyectos = [
     {
         link: "./projects/mockup",
-        img:"src/imgs/mockup.png",
-        title: "Mockup",
-        timeSpent: "3 h",
+        img:"src/imgs/SudokuProject.png",
+        title: "Sudoku solver",
+        timeSpent: "7 h",
         bio: "biologica",
         tsName: "Mockup"
     },
 
     {
-        link: "#",
+        link: "./projects/fpquest",
         img:"src/imgs/fpquestProject.png",
         title: "FpQuest",
         timeSpent: "504 h",
-        bio: "Small game made using unity engine. Based in The Binding of Isaac"
+        bio: "Small game made using unity engine. Based in The Binding of Isaac",
+        tsName: "FpQuest"
     }
 
     
